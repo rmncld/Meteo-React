febcee7620e63c74d700d6120c2b5777cbc88d2c
rmncld <colard.romain@gmail.com>
2022-09-09 13:49:16 +0200

    creation .env

--- src/components/Meteowidget/MeteoWidget.js
+++ src/components/Meteowidget/MeteoWidget.js
@@ -5,28 +5,44 @@ import './MeteoWidget.scss';
 
 function MeteoWidget({ city, code }) {
 
-    const [temperature, setTemperature] = useState(null);
-    const [icon, setIcon] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [icon, setIcon] = useState(null);
 
-useEffect(() => {
-    const API_KEY = '46cb5e69bc5cca4f3b28d6148e09e26f';
-    axios.get( `https://api.openweathermap.org/data/2.5/weather?zip=${code},fr&appid=${API_KEY}&units=metric`)
-    .then((response) => {
+  useEffect(() => {
+
+    /** On place les données sensibles dans les variables d'environnement 
+     * - .env : 
+     * REACT_APP_API_KEY=47840f4f526d9cc69b4b575c52495860
+     * 
+     * - .env.development et .env.production
+     * 
+     * Les variables qui commencent par REACT_APP_xxx sont placées automatiquement
+     * dans une variable globale process.env
+     * https://create-react-app.dev/docs/adding-custom-environment-variables/
+     * 
+     * On pourrait faire une condition sur l'environnement (process.env.NODE_ENV)
+     * pour dire si on est de dev on utilise l'API locale
+     * et si on est en prod on utilise l'API openweather
+    */
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    console.log(API_KEY)
+    
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${code},fr&appid=${API_KEY}&units=metric`)
+      .then((response) => {
         setTemperature(Math.round(response.data.main.temp));
         setIcon(response.data.weather[0].icon);
-    });
-}, [code]);
-
-
+      });
+  }, []);
 
   return (
     <div className='meteo'>
-         <div>
+      <div>
         <div className='meteo-city'>{city}</div>
         <div className='meteo-code'>{code}</div>
         <div className='meteo-temperature'>{temperature} °</div>
       </div>
-      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+      
+      {icon && <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />}
     </div>
   )
 };
