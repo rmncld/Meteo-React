c9e1e1d1cb409bc3fc144a3e9913517d462c4772
rmncld <colard.romain@gmail.com>
2022-09-09 13:27:07 +0200

    meteo-react

--- src/components/Meteowidget/MeteoWidget.js
+++ src/components/Meteowidget/MeteoWidget.js
@@ -1,24 +1,32 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import './MeteoWidget.scss';
 
 function MeteoWidget({ city, code }) {
+
+    const [temperature, setTemperature] = useState(null);
+    const [icon, setIcon] = useState(null);
 
 useEffect(() => {
     const API_KEY = '46cb5e69bc5cca4f3b28d6148e09e26f';
     axios.get( `https://api.openweathermap.org/data/2.5/weather?zip=${code},fr&appid=${API_KEY}&units=metric`)
     .then((response) => {
-        console.log(response.data.main.temp);
-    })
-}, []);
+        setTemperature(Math.round(response.data.main.temp));
+        setIcon(response.data.weather[0].icon);
+    });
+}, [code]);
 
 
 
   return (
     <div className='meteo'>
-      <div className='meteo-city'>{city}</div>
-      <div className='meteo-code'>{code}</div>
-      <div className='meteo-temperature'>15 °</div>
+         <div>
+        <div className='meteo-city'>{city}</div>
+        <div className='meteo-code'>{code}</div>
+        <div className='meteo-temperature'>{temperature} °</div>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
     </div>
   )
 };
